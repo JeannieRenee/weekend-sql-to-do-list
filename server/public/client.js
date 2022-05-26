57cec74ae029554577a1234110c6616bd04c62b8
Jeannie Branstrator <jmbranstrator@gmail.com>
2022-05-26 17:38:30 -0500

    files created and linked

---
+++ server/public/client.js
@@ -0,0 +1,14 @@
+// Linked Successfully
+console.log ('linked js'); 
+
+$(document).ready(onReady);
+
+function onReady() {
+    clickHandlers();
+}
+
+function clickHandlers() {
+    // $('#submitBtn').on('click', handleSubmit);
+    // $(document).on('click', '.deleteButton', deleteBook);
+    // $(document).on('click', '.isRead', updateisRead);
+}
