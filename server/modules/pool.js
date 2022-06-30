1b5d1b166e63ea790bb8097e725be2405eacd8c5
Jeannie Branstrator <jmbranstrator@gmail.com>
2022-06-30 15:48:14 -0500

    pool

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,12 +1,23 @@
 const pg = require('pg');
+const url = require('url');
 
-const config = {
-  database: process.env.DATABASE_NAME || 'weekend-to-do-app', 
-  host: 'localhost', 
-  port: 5432, 
-  max: 10, 
-  idleTimeoutMillis: 30000 
-};
+let config = {};
+
+if (process.env.DATABASE_URL) {
+  config = {
+    // We use the DATABASE_URL from Heroku to connect to our DB
+    connectionString: process.env.DATABASE_URL,
+    // Heroku also requires this special `ssl` config
+    ssl: { rejectUnauthorized: false },
+  };
+} else {
+  // If we're not on heroku, configure PG to use our local database
+  config = {
+    host: 'localhost',
+    port: 5432,
+    database: 'weekend-to-do-app', // CHANGE THIS LINE to match your local database name!
+  };
+}
 
 const pool = new pg.Pool(config);
 
